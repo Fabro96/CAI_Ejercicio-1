0f887fe61672c2f5be6cb105875ecbc791993a91
Fabrizio Agustín Ghilardini <70611279+Fabro96@users.noreply.github.com>
2020-09-06 10:52:41 -0300

    Objeto Persona 1
    Se instanció el objeto p1 con sus correspondientes datos

--- Ejercicio 1 - CAI/Ejercicio 1 - CAI/Persona.cs
+++ Ejercicio 1 - CAI/Ejercicio 1 - CAI/Persona.cs
@@ -8,5 +8,55 @@ namespace Ejercicio_1___CAI
 {
     class Persona
     {
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+        
+
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+            set
+            {
+                this._nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return this._apellido;
+            }
+            set
+            {
+                this._apellido = value;
+            }
+        }
+
+        public string NombreCompleto
+        {
+            get
+            {
+                return this._nombre + " " + this._apellido;
+            }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return _edad;
+            }
+            set
+            {
+                _edad = value;
+
+            }
+        }
+
     }
 }
--- Ejercicio 1 - CAI/Ejercicio 1 - CAI/Program.cs
+++ Ejercicio 1 - CAI/Ejercicio 1 - CAI/Program.cs
@@ -10,6 +10,16 @@ namespace Ejercicio_1___CAI
     {
         static void Main(string[] args)
         {
+            Persona p1 = new Persona();
+
+            p1.Nombre = "Fabrizio";
+            p1.Apellido = "Ghilardini";
+            p1.Edad = 23;
+
+            Console.WriteLine("Nombre Completo: " + p1.NombreCompleto);
+            Console.WriteLine("Edad: " + p1.Edad.ToString());
+
+            Console.ReadKey();
         }
     }
 }
